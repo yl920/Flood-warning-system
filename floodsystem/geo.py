29939bbe5593fd1ffea6a66b85a0b510c664653d
andy <kyl40@cam.ac.uk>
2023-01-31 14:59:23 +0000

    new file:   Task1B.py modified:   floodsystem/geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,5 +5,20 @@
 geographical data.
 
 """
+#from floodsystem.stationdata import build_station_list
 
-from .utils import sorted_by_key  # noqa
+#from .utils import sorted_by_key  # noqa
+import operator
+from haversine import haversine, Unit
+list=[]
+def stations_by_distance(stations, p):
+    for i in  stations:
+        station_name = i["Station name"]
+        coords = i["coordinate"]
+        d = haversine(p,coords)
+        list.append(())
+        list(len(list)-1).append(station_name)
+        list(len(list)-1).append(d)
+    sorted_by_key(list,1)
+
+#print(stations_by_distance())
