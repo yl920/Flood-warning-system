aaa62bf8ed408c2092e51e8f66ea66566b12d3f0
Mary <yl920@cam.ac.uk>
2023-01-31 15:53:12 +0000

    modified:   Task1C.py

--- Task1C.py
+++ Task1C.py
@@ -2,3 +2,19 @@
 The required function signature is:
             def stations_within_radius(stations, centre, r):
 where stations is a list of MonitoringStation objects, centre is the coordinate x and r is the radius.'''
+
+from floodsystem.stationdata import build_station_list
+
+def stations_within_radius(stations, centre, r):
+    init_stations = build_station_list()
+
+    for station in init_stations:
+        if station.name in stations:
+            return station
+    
+   # if station.
+
+
+    
+
+
