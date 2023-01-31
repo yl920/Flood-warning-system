80a89f4c5cc1452521595191796ce344891b0060
andy <kyl40@cam.ac.uk>
2023-01-31 16:17:59 +0000

    modified:   Task1B.py

--- Task1B.py
+++ Task1B.py
@@ -1,5 +1,5 @@
-from floodsystem import geo
+from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 data = build_station_list(use_cache=True)
-p = (52.2053, 0.1218)
-print(geo.stations_by_distance(data, p))
+f = (52.2053, 0.1218)
+print(stations_by_distance(data, (52.2053, 0.1218)))
