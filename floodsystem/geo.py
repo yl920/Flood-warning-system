3b53e4bd18c1c0d9c77e47057e845cb6073cba94
Mary <yl920@cam.ac.uk>
2023-02-01 07:48:28 +0000

    modified:   floodsystem/geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -19,3 +19,32 @@ def stations_by_distance(stations, p):
         list(len(list)-1).append(station_name)
         list(len(list)-1).append(d)
     sorted_by_key(list,1)
+
+
+### 1C ###
+
+from math import asin, sqrt, sin, cos, radians
+
+def haversine(a, b):
+    r = 6371
+    a_lat, a_lng = a[0], a[1]
+    b_lat, b_lng = b[0], b[1]
+
+    return 2 * r * asin(
+        sqrt(sin(radians((b_lat - a_lat) / 2)) ** 2
+             + cos(radians(a_lat))
+             * cos(radians(b_lat))
+             * sin(radians((b_lng - a_lng) / 2)) ** 2
+             )
+    )
+
+
+def stations_within_radius(stations, centre, r):
+    
+    stations_in = []
+
+    for station in stations:
+        centre_distance = haversine(centre, station.coord)
+        if centre_distance <= r:
+            stations_in.append(station.name)
+    return stations_in
