e558da20f19a145dc7729687e57617c6032fae2a
Mary <yl920@cam.ac.uk>
2023-02-02 21:41:05 +0000

    modified:   Task1E.py

--- Task1E.py
+++ Task1E.py
@@ -8,4 +8,20 @@ In the case that there are more rivers with the same number of stations as the N
 '''
 
 from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
 
+def run():
+
+    stations = build_station_list
+    N = 9
+
+    river_station_number = rivers_by_station_number(stations, N)
+
+    print(f'The {N} rivers with greatest number of stations :')
+    print(river_station_number)
+
+if __name__ == "__main__":
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
+    run() 
+
+    
