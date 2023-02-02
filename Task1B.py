239d895a3adf969fb1e498f8b26db6d6dee7d4cc
andy <kyl40@cam.ac.uk>
2023-02-02 21:16:53 +0000

    modified:   .github/workflows/pythonapp.yml 	modified:   Task1B.py 	renamed:    floodsystem/__init__.py -> floodsystem/__init__ 	modified:   floodsystem/geo.py

--- Task1B.py
+++ Task1B.py
@@ -4,17 +4,14 @@ from floodsystem.stationdata import build_station_list
 
 
 def run():
-
-
-
     stations = build_station_list() 
-
+#Define a list of station that is sorted by distance to Cambridge
     new_list = stations_by_distance(stations,(52.2053, 0.1218))
     print('The closest 10 entries are:')
+#Print the first and last 10 entries respectively
     print(new_list[:10])
     print('The furthest 10 are:')
     print(new_list[-10:])
-
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
     run()
