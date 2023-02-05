a19dacab545867105ee5be16df3f15d9ed756351
andy <kyl40@cam.ac.uk>
2023-02-05 01:16:29 +0000

    modified:   Task1B.py 	modified:   Task1C.py 	modified:   Task1D.py 	modified:   Task1E.py 	modified:   Task1F.py 	modified:   floodsystem/geo.py 	modified:   test_station.py

---
+++ test_geo.py
@@ -0,0 +1,13 @@
+from floodsystem.geo import *
+from floodsystem.stationdata import *
+import pytest
+"""...
+def test_stations_by_distance():
+    stations = build_station_list
+    test = stations_by_distance(stations, (52.2053, 0.1218))
+    return test[:2]
+print(test_stations_by_distance())
+#assert test_stations_by_distance == ('Cambridge Jesus Lock', 'Cambridge', 0.8402364350834112), ('Bin Brook', 'Cambridge', 2.5022740869515525)
+"""
+
+
