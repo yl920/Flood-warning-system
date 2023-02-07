83887638d952f8502b9c186e73bfb40ddb08c5a5
yl920 <115568580+yl920@users.noreply.github.com>
2023-02-07 13:40:22 +0000

    modified:   test_geo.py modified:   test_station.py

--- test_station.py
+++ test_station.py
@@ -3,8 +3,10 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
-import pytest
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
+
+
+# import pytest
 
 def test_create_monitoring_station():
 
@@ -27,49 +29,23 @@ def test_create_monitoring_station():
     assert s.town == town
 
 #Test Task1F
-def test_typical_range_consistent(self):
-    assert MonitoringStation("111", "111", "station1", (0., 1.), (1., 4.), "River 1",
-                                 "Town 1").typical_range_consistent() == True
-    assert MonitoringStation("111", "111", "station1", (0., 1.), None, "River 1",
-                                 "Town 1").typical_range_consistent() == False
-    assert MonitoringStation("111", "111", "station1", (0., 1.), (1, -4), "River 1",
-                                 "Town 1").typical_range_consistent() == False
+def test_inconsistent_typical_range_stations():
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange_A = (-2.3, 3.4445)
+    trange_B = None
+    trange_C = (3.4445, -2.3)
+    river = "River X"
+    town = "My Town"
+    s1 = MonitoringStation(s_id, m_id, label, coord, trange_A, river, town)
+    s2 = MonitoringStation(s_id, m_id, label, coord, trange_B, river, town)
+    s3 = MonitoringStation(s_id, m_id, label, coord, trange_C, river, town)
 
-
-from floodsystem.station import inconsistent_typical_range_stations
-def test_inconsistent_typical_range_stations(self):
-        station1 = MonitoringStation(station_id="111",
-                                     measure_id="111",
-                                     label="Station A",
-                                     coord=(0, 1),
-                                     typical_range=(1., 4.),
-                                     river="River 1",
-                                     town="Town 1")
-        station2 = MonitoringStation(station_id="222",
-                                     measure_id="222",
-                                     label="Station B",
-                                     coord=(1, 1),
-                                     typical_range=None,
-                                     river="River 2",
-                                     town="Town 2")
-        station3 = MonitoringStation(station_id="333",
-                                     measure_id="333",
-                                     label="Station C",
-                                     coord=(0, 3),
-                                     typical_range=(1, -4),
-                                     river="River 2",
-                                     town="Town 3")
-        station4 = MonitoringStation(station_id="444",
-                                     measure_id="444",
-                                     label="Station D",
-                                     coord=(8, 3),
-                                     typical_range=(0.5, 0.2),
-                                     river="River 3",
-                                     town="Town 4")
-        assert inconsistent_typical_range_stations([station1]) == []
-        assert inconsistent_typical_range_stations([station1, station2]) == [station2.name]
-        assert inconsistent_typical_range_stations([station2, station3]) == [station2.name, station3.name]
-        assert inconsistent_typical_range_stations([station4, station2, station3]) == [station2.name, station3.name,
-                                                                                       station4.name]
-
+    stations = [s1, s2, s3]
+    data = inconsistent_typical_range_stations(stations)
+    assert s1 not in data
+    assert s2 in data
+    assert s3 in data
 
