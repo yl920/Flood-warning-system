a1d330636c1e171e4634a3c12831cbc13c5c5982
Mary <yl920@cam.ac.uk>
2023-02-15 22:08:44 +0000

    modified:   floodsystem/flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -5,9 +5,10 @@ def stations_level_over_threshold(stations, tol):
     qualified_list = []
     station_in_tol = ()
     for station in stations:
-        if MonitoringStation.relative_water_level(station) > tol:
+        relative_fraction = MonitoringStation.relative_water_level(station)
+        if relative_fraction > tol:
             station_in_tol[0] = station.name
-            station_in_tol[1] = MonitoringStation.relative_water_level(station)
+            station_in_tol[1] = relative_fraction
         
         qualified_list += station_in_tol
     
