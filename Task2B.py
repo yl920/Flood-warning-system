b8d779ef4669c75bd6cdbd2b280e07188261421d
Mary <yl920@cam.ac.uk>
2023-02-15 22:00:07 +0000

    modified:   Task2B.py 	modified:   floodsystem/stationdata.py

--- Task2B.py
+++ Task2B.py
@@ -8,8 +8,8 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 
 def run():
     
-    stations = build_station_list
-    #update_water_levels(stations)
+    stations = build_station_list()
+    update_water_levels(stations)
 
     tol = 0.8
     s=stations_level_over_threshold(stations, tol)
