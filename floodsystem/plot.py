48c418a85c4ed372c25f1eff1ad3da34a35b35ce
Mary <yl920@cam.ac.uk>
2023-02-22 15:11:31 +0000

    modified:   floodsystem/plot.py 	new file:   test_flood.py 	new file:   test_plot.py

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,10 +1,11 @@
-import matplotlib as plt
-
+import matplotlib 
+from .station import MonitoringStation
+import matplotlib.pyplot as plt
 
 def plot_water_levels(station, dates, levels):
 
     #plot
-    plt.plot(dates, levels)
+    plt.plot(dates, levels, label = "water level data")
 
     #Add axis labels, title
     plt.title(station.name)
@@ -17,10 +18,12 @@ def plot_water_levels(station, dates, levels):
     low = [station.typical_range[0]] * len(levels)
     plt.plot(dates, high, '--',label = "typical high level")
     plt.plot(dates, low, 'g--',label = "typical low level")   
-
+    plt.legend()
+    
     #display plot
     plt.tight_layout()
 
     plt.show
-
+    
     return
+
