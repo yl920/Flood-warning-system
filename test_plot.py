a1568c0498ed8a58ee886dfdabab07fc949d57b8
Mary <yl920@cam.ac.uk>
2023-02-27 12:41:57 +0000

    modified:   test_flood.py modified:   test_plot.py

--- test_plot.py
+++ test_plot.py
@@ -1,4 +1,41 @@
 import matplotlib.pyplot as plt
-from floodsystem.plot import *
-from floodsystem.station import MonitoringStation
+from floodsystem.plot import plot_water_levels
+import datetime
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import MonitoringStation
+import numpy as np
 
+class TestClass:
+    def test_plot_water_levels(self): #Mary 2E
+        station1 = MonitoringStation(station_id='test_station_id_1',
+                                     measure_id='test_measure_id_1',
+                                     label='Test Station 1',
+                                     coord=(0., 1.),
+                                     typical_range=(0., 1.),
+                                     river='test_river_1',
+                                     town='test_town_1')
+        station2 = MonitoringStation(station_id='test_station_id_2',
+                                     measure_id='test_measure_id_2',
+                                     label='Test Station 2',
+                                     coord=(1., 1.),
+                                     typical_range=(0., 1.),
+                                     river='test_river_2',
+                                     town='test_town_2')
+        station3 = MonitoringStation(station_id='test_station_id_3',
+                                     measure_id='test_measure_id_3',
+                                     label='Test Station 3',
+                                     coord=(1., 1.),
+                                     typical_range=(0., 1.),
+                                     river='test_river_3',
+                                     town='test_town_3')
+        stations = (station1, station2, station3)
+        dates = np.empty(5, dtype=object)
+        levels = np.empty(5, dtype=object)
+        dt = 10
+        for i in range(len(stations)):
+            dates[i], levels[i] = fetch_measure_levels(
+                stations[i].measure_id, dt=datetime.timedelta(days=dt))
+
+            plot_water_levels(stations[i], dates[i], levels[i])
+
+        assert plt.show() == None
