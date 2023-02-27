777d6fea2580be533723e48b0d9a1571d2ffb25d
andy <kyl40@cam.ac.uk>
2023-02-27 23:40:29 +0000

    modified:   Task2G.py

--- Task2G.py
+++ Task2G.py
@@ -7,19 +7,19 @@ import matplotlib.dates
 from floodsystem.station import *
 
 def run():
-    no_data = []#create empty list of stations with different catergories
+    null = []#create empty list of stations with different catergories
     low = []
-    moderate = []
+    mod = []
     high = [] 
     severe = []
     stations = build_station_list()
     update_water_levels(stations)
-    threshold_severe = 1.2 #threshold
-    threshold_high = 1.05
-    threshold_mod = 80
-
+    threshold_severe = float(input("severe")) #threshold
+    threshold_high = float(input("high"))
+    threshold_mod = float(input("mod"))
+    print("placeholder")
     for station in stations:#calculate range with typical data
-        if station.typical_range_consistent() and station.latest_level is not None:
+        if station.typical_range_consistent() and station.latest_level is not None: #filter out inconsistent data
 
             # The function that calculates the risk of flooding is as follows: 
             # it takes the percentage difference between what the water level at the station is now 
@@ -31,53 +31,64 @@ def run():
             # 80% - 105% - moderate risk
             # <80% - low risk
 
-            percentage = station.relative_water_level(station.latest_level)
-
+            rel=station.relative_water_level(station.latest_level)
+            subject = station.town
             # Some stations have .town = None
             # Only include these stations if the flood warning is high/severe
-            if station.town is None and percentage >= 1.2:
-                station.town = f"The following station does not have data: {station.name}"
-
-            if station.town in severe:
-                pass
-            elif station.town in high:
-                if percentage >= threshold_severe:
-                    severe.append(station.town)
-                    high.remove(station.town)
-            elif station.town in moderate:
-                if percentage >= threshold_severe:
-                    severe.append(station.town)
-                    moderate.remove(station.town)
-                elif percentage >= threshold_high:
-                    high.append(station.town)
-                    moderate.remove(station.town)
-            elif station.town in low:
-                if percentage >= threshold_severe:
-                    severe.append(station.town)
-                    low.remove(station.town)
-                elif percentage >= threshold_high:
-                    high.append(station.town)
-                    low.remove(station.town)
-                elif percentage >= threshold_mod:
-                    moderate.append(station.town)
-                    low.remove(station.town)
+            if subject is None and rel >= threshold_high: #for stations that have station.town=None and at high or more risk...
+                subject = f"The following station does not have data: {station.name}"#...raise warning indicating unnamed at-risk station
+            if rel >= threshold_severe:
+                severe.append(subject)
+            elif rel < threshold_severe and rel >= threshold_high:
+                high.append(subject)
+            elif rel < threshold_high and rel >= threshold_mod:
+                mod.append(subject)
+            elif rel < threshold_mod:
+                low.append(subject)
             else:
-                if percentage >= threshold_severe:
-                    severe.append(station.town)
-                elif percentage >= threshold_high:
-                    high.append(station.town)
-                elif percentage >= threshold_mod:
-                    moderate.append(station.town)
-                elif percentage < threshold_mod:
-                    low.append(station.town)
-                else: no_data.append(station.town)
+                null.append(subject)
+            """
+            if subject in severe:#if the subject is already labelled severe, move on...
+                pass
+            elif subject in high:
+                if rel >= threshold_severe:#if the subject exceeds severe threshold, remove from high and add to severe
+                    severe.append(subject)
+                    high.remove(subject)
+            elif subject in mod:
+                if rel >= threshold_severe:#if the subject exceeds severe threshold, remove from moderate and add to severe
+                    severe.append(subject)
+                    mod.remove(subject)
+                elif rel >= threshold_high:#...similar to above
+                    high.append(subject)
+                    mod.remove(subject)
+            elif subject in low:
+                if rel >= threshold_severe:
+                    severe.append(subject)
+                    low.remove(subject)
+                elif rel >= threshold_high:
+                    high.append(subject)
+                    low.remove(subject)
+                elif rel >= threshold_mod:
+                    mod.append(subject)
+                    low.remove(subject)
+            else:#if subject is not already placed in a list, 
+                if rel >= threshold_severe:#...still the same
+                    severe.append(subject)
+                elif rel >= threshold_high:
+                    high.append(subject)
+                elif rel >= threshold_mod:
+                    mod.append(subject)
+                elif rel < threshold_mod:#if the subject is not in any list but still is not None, that implies subject has value lower than moderate threshold and placed in low risk
+                    low.append(subject)
+                else: null.append(subject)#otherwise append subject to list with faulty data
+                """
         else:
             pass
         
         # For stations identified as high risk, estimate if water level
         # is rising or falling using the polyfit best fit function
         # Then, if water level rising, upgrade warning level to severe
-        if station.town in high:
+        if subject in high:
             try:
                 # Create best-fit polynomial to approximate water level data over past 5 days
                 measure_id = station.measure_id
@@ -88,8 +99,8 @@ def run():
                 gradient = derivative(matplotlib.dates.date2num(dates[0]) - offset) # Evaluate gradient
                 # Gradients which are positive but close to 0 are ignored here
                 if gradient > 1:
-                    high.remove(station.town)
-                    severe.append(station.town)
+                    high.remove(subject)
+                    severe.append(subject)
             # Some stations have faulty data - no useful polynomial fit, so leave in current warning category
             except IndexError:
                 pass
@@ -101,9 +112,9 @@ def run():
 
     print(f"Severe Risk:\n{severe}\n")
     print(f"High Risk:\n{high}\n")
-    print(f"Moderate Risk:\n{len(moderate)} towns\n")
+    print(f"Moderate Risk:\n{len(mod)} towns\n")
     print(f"Low Risk:\n{len(low)} towns\n")
-    print(f"No Reliable Data:\n{len(no_data)} towns\n")
+    print(f"No Reliable Data:\n{len(null)} towns\n")
 
 if __name__ == "__main__":
     run()
