bb09dcb1a87833b4c4895b83db04fe5d8e9e1a0f
andy <kyl40@cam.ac.uk>
2023-02-27 22:07:56 +0000

    modified:   Task2G.py

--- Task2G.py
+++ Task2G.py
@@ -5,21 +5,20 @@ import numpy as np
 import datetime
 import matplotlib.dates
 from floodsystem.station import *
-from floodsystem.station import MonitoringStation
 
 def run():
-    severe = []
-    high = []
-    moderate = []
+    no_data = []#create empty list of stations with different catergories
     low = []
-    no_data = []
+    moderate = []
+    high = [] 
+    severe = []
     stations = build_station_list()
     update_water_levels(stations)
+    threshold_severe = 1.2 #threshold
+    threshold_high = 1.05
+    threshold_mod = 80
 
-    for station in stations:
-
-        # Risk can only be calculated if there is typical range to compare to
-
+    for station in stations:#calculate range with typical data
         if station.typical_range_consistent() and station.latest_level is not None:
 
             # The function that calculates the risk of flooding is as follows: 
@@ -27,49 +26,49 @@ def run():
             # and the averaged water level at the station over the last 24 hours
 
             # If the percentage of water level is compared to the previous day:
-            # >150%  - severe risk
-            # 120% - 150%  - high risk
-            # 90% - 120% - moderate risk
-            # <90% - low risk
+            # >120%  - severe risk
+            # 105% - 120%  - high risk
+            # 80% - 105% - moderate risk
+            # <80% - low risk
 
             percentage = station.relative_water_level(station.latest_level)
 
             # Some stations have .town = None
             # Only include these stations if the flood warning is high/severe
             if station.town is None and percentage >= 1.2:
-                station.town = "No town data - station: " + station.name
-            
+                station.town = f"The following station does not have data: {station.name}"
+
             if station.town in severe:
                 pass
             elif station.town in high:
-                if percentage >= 1.5:
+                if percentage >= threshold_severe:
                     severe.append(station.town)
                     high.remove(station.town)
             elif station.town in moderate:
-                if percentage >= 1.5:
+                if percentage >= threshold_severe:
                     severe.append(station.town)
                     moderate.remove(station.town)
-                elif percentage >= 1.2:
+                elif percentage >= threshold_high:
                     high.append(station.town)
                     moderate.remove(station.town)
             elif station.town in low:
-                if percentage >= 1.5:
+                if percentage >= threshold_severe:
                     severe.append(station.town)
                     low.remove(station.town)
-                elif percentage >= 1.2:
+                elif percentage >= threshold_high:
                     high.append(station.town)
                     low.remove(station.town)
-                elif percentage >= 0.9:
+                elif percentage >= threshold_mod:
                     moderate.append(station.town)
                     low.remove(station.town)
             else:
-                if percentage >= 1.5:
+                if percentage >= threshold_severe:
                     severe.append(station.town)
-                elif percentage >= 1.2:
+                elif percentage >= threshold_high:
                     high.append(station.town)
-                elif percentage >= 0.9:
+                elif percentage >= threshold_mod:
                     moderate.append(station.town)
-                elif percentage < 0.9:
+                elif percentage < threshold_mod:
                     low.append(station.town)
                 else: no_data.append(station.town)
         else:
