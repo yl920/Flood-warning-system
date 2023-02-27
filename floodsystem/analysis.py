79956a74344eb3651a6d549f9fb7ca713bff44c2
andy <kyl40@cam.ac.uk>
2023-02-27 15:29:07 +0000

    modified:   floodsystem/station.py

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,8 +1,9 @@
 import numpy as np
-from matplotlib.dates import date2num
-
-def polyfit(dates, levels, p):
-    datenum = date2num(dates)
-    x = [date - datenum[-1] for date in datenum]
-    poly = np.poly1d(np.polyfit(x, levels, p))
-    return poly, datenum[-1]
+import matplotlib.dates
+#Andy
+def polyfit(dates, levels, p):#computes a least-squares fit of a polynomial of degree p to water level data
+    x = matplotlib.dates.date2num(dates)
+    large = x[0] #the shift to account for error, since the number of days from the start of calender is very large
+    pcof=np.polyfit(x - large, levels, p) #Using shifted x values, find coefficient of best-fit
+    poly = np.poly1d(pcof)# Convert coefficient into a polynomial that can be evaluated
+    return poly, large #returns the coefficient for the polynomial, and the shift in days
